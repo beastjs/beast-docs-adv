986951870d8d437c4e5a28317e58abab8b0392e7
phtn <phtn458@gmail.com>
2026-09-13 19:36:46 +0800

    update:tweaks

--- src/lib/icons/icons.ts
+++ src/lib/icons/icons.ts
@@ -43,7 +43,7 @@ export const icons = {
   },
   'arrow-right': {
     symbol:
-      '<g transform="scale(0.667)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-4 4 4-4m-4-4 4 4"/></g>',
+      '<g transform="scale(0.667)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-6 6 6-6m-6-6 6 6"/></g>',
     viewBox: '0 0 16 16',
     set: 'svg'
   },
@@ -492,6 +492,12 @@ export const icons = {
     symbol:
       '<g transform="scale(1.75)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.875 6.27c.7.398 1.13 1.143 1.125 1.948v7.284c0 .809-.443 1.555-1.158 1.948l-6.75 4.27a2.27 2.27 0 0 1-2.184 0l-6.75-4.27A2.23 2.23 0 0 1 3 15.502V8.217c0-.809.443-1.554 1.158-1.947l6.75-3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98zM12 8.5v7M9 10l6 4m-6 0 6-4"/></g>',
     viewBox: '0 0 42 42',
+    set: 'svg'
+  },
+  terminal: {
+    symbol:
+      '<g transform="scale(0.667)"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 7 5 5-5 5m7 2h7"/></g>',
+    viewBox: '0 0 16 16',
     set: 'svg'
   }
 }
--- src/lib/status.ts
+++ src/lib/status.ts
@@ -1,8 +1,8 @@
 export function getProjectStatus(version: string) {
   return `┌─ BEAST ────────────┐
- │ source   .btsx     │
- │ output   .tsrx     │
- │ runtime  octane    │
- │ status   ${version.padEnd(10).slice(0, 10)}│
- └────────────────────┘`
+│ source   .btsx     │
+│ output   .tsrx     │
+│ runtime  octane    │
+│ status   ${version.padEnd(10).slice(0, 10)}│
+└────────────────────┘`
 }
